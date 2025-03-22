03d8f45aa1dc1df6565963fad6b9e38f493c5ed9
Shekhar Suman <shekharsuman@Shekhar-ka-Mac-mini.local>
2025-03-22 15:00:18 +0530

    1st Commit

---
+++ src/lambda/download-image/lambda-function.py
@@ -0,0 +1,47 @@
+import boto3
+import json
+import os
+
+s3 = boto3.client("s3")
+dynamodb = boto3.resource("dynamodb")
+
+TABLE_NAME = os.environ["TABLE_NAME"]
+BUCKET_NAME = os.environ["BUCKET_NAME"]
+
+table = dynamodb.Table(TABLE_NAME)
+
+def lambda_handler(event, context):
+    try:
+        image_id = event.get("queryStringParameters", {}).get("imageId")
+
+        # For given image id in parameter , get that item from DynaoDB table
+        response = table.get_item(Key={"imageId": image_id})
+
+        if "Item" not in response:
+            return {
+                "statusCode": 404,
+                "body": json.dumps({"error": "Image not found"})
+            }
+
+        key = response["Item"]["s3_key"]
+
+        # Generate a pre-signed URL for download so that using this pre-signed URL user can download image
+        presigned_url = s3.generate_presigned_url(
+            'get_object',
+            Params={'Bucket': BUCKET_NAME, 'Key': key},
+            ExpiresIn=600  # 10 mins
+        )
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "download_url": presigned_url,
+                "message": "Use this URL to download the image. Link is valid for 10 minutes."
+            })
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
---
+++ src/lambda/filter-image/lambda-function.py
@@ -0,0 +1,47 @@
+import json
+import boto3
+import os
+from boto3.dynamodb.conditions import Attr
+
+dynamodb = boto3.resource("dynamodb")
+TABLE_NAME = os.environ["TABLE_NAME"]
+table = dynamodb.Table(TABLE_NAME)
+
+def lambda_handler(event, context):
+    try:
+        location = event.get("queryStringParameters", {}).get("location")
+        tag = event.get("queryStringParameters", {}).get("tag")
+
+        # Build filter expression
+        filter_expr = None
+
+        '''
+        this can also be optimized based on like condition for example 
+        user typed Ind he can see India , Indonesia or other starting with Ind...
+        '''
+    
+        if location:
+            filter_expr = Attr("location").eq(location)
+        if tag:
+            tag_expr = Attr("tag").contains(tag)
+            filter_expr = tag_expr if not filter_expr else filter_expr & tag_expr
+
+        # Scan with or without filters
+        if filter_expr:
+            response = table.scan(FilterExpression=filter_expr)
+        else:
+            response = table.scan()
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "results": response["Items"],
+                "count": len(response["Items"])
+            })
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
